6a2d168cbaa4fa3a0cbfbaca915945c33e462272
ronnnnie85 <ronnnnie85@gmail.com>
2025-02-26 12:34:44 +0700

    написал основные функции

--- src/widget.py
+++ src/widget.py
@@ -1,6 +1,30 @@
+import re
+from datetime import datetime
+from masks import get_mask_card_number, get_mask_account
+
 def mask_account_card(account_card_data: str) -> str:
-    pass
+    """Обрабатывает информацию о картах и счетах"""
+    match = re.search(r'(\D+)\s(\d+)', account_card_data) # Получаю группировки строки по шаблону (буквы) (цифры)
+
+    if match is None:
+        return ""
+
+    prefix, number = match.groups()
+
+    if prefix.lower().startswith("счет"): # Первым идет Счет, значит обрабатываем как счет
+        masked_number = get_mask_account(int(number))
+    elif len(number) == 16: # Обрабатываем как карту
+        masked_number = get_mask_card_number(int(number))
+    else:
+        return ""
+
+    return f'{prefix} {masked_number}'
 
 
 def get_date(raw_date: str) -> str:
-    pass
+    """Конвертирует строку с датой из формата ISO 8601 в DD-MM-YYYY"""
+    dt = datetime.strptime(raw_date, "%Y-%m-%dT%H:%M:%S.%f") # Получаем из строки объект datetime по шаблону
+
+    return dt.strftime("%d.%m.%Y") # Преобразуем в нужный формат в строку
+
+
