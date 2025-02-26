74e2b4b132de80cd270467456fcefefeb7b596f5
ronnnnie85 <ronnnnie85@gmail.com>
2025-02-26 13:08:28 +0700

    После проверки линтерами

--- tests/test.py
+++ tests/test.py
@@ -2,7 +2,7 @@ from src.widget import get_date, mask_account_card
 
 print(mask_account_card("Maestro 1596837868705199"))
 print(mask_account_card("Счет 64686473678894779589"))
-print(mask_account_card("MasterCard 7158300734726758"))
+print(mask_account_card("MasterCard 715830073476758"))
 print(mask_account_card("Счет 35383033474447895560"))
 print(mask_account_card("Visa Platinum 8990922113665229"))
 print(mask_account_card("Visa Gold 5999414228426353"))
