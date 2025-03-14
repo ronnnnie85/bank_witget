e59ebeda34f03c1ab8b834bd1d258242badee07b
ronnnnie85 <ronnnnie85@gmail.com>
2025-03-14 19:38:09 +0700

    сделал все тесты

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,4 @@
+import re
 from datetime import datetime
 from typing import Union
 
@@ -6,20 +7,8 @@ def filter_by_state(
     lst_dct: list[dict[str, Union[str, int]]], state: str = "EXECUTED"
 ) -> list[dict[str, Union[str, int]]]:
     """Принимает список и ключ, возвращает отфильтрованный список"""
-    if not isinstance(lst_dct, list):
+    if is_wrong_parameter(lst_dct):
         raise TypeError("Ошибка: некорректный тип входных данных.")
-    else:
-        for dct in lst_dct:
-            if not isinstance(dct, dict):
-                raise TypeError("Ошибка: некорректный тип входных данных.")
-            else:
-                for key, value in dct.items():
-                    if not (
-                        type(value) is str or type(value) is int
-                    ) or not isinstance(key, str):
-                        raise TypeError(
-                            "Ошибка: некорректный тип входных данных."
-                        )
 
     # Использую генератор списка для фильтрации
     return [dct for dct in lst_dct if dct.get("state", "") == state]
@@ -29,6 +18,22 @@ def sort_by_date(
     lst_dct: list[dict[str, Union[str, int]]], descending: bool = True
 ) -> list[dict[str, Union[str, int]]]:
     """Принимает список и ключ, возвращает отсортированный список"""
+    pattern = (
+        r"[0-9][0-9](0[1-9]|[1-9][0-9])\-(0[1-9]|1[0-2])-"
+        r"(0[1-9]|[1-2][0-9]|3[0-1])"
+        r"T([0-1][0-9]|2[0-3]):([0-4][0-9]|5[0-9]):([0-4][0-9]|5[0-9])"
+        r"(\.[0-9][0-9][0-9][0-9](0[1-9]|[1-9][0-9])){0,1}"
+    )
+
+    if is_wrong_parameter(lst_dct):
+        raise TypeError("Ошибка: некорректный тип входных данных.")
+
+    for dct in lst_dct:
+        if not dct.get("date") is None:
+            match = re.search(pattern, str(dct["date"]))
+            if match is None:
+                raise ValueError("Ошибка: некорректный формат даты.")
+
     # Сортирую, ключ - объект datetime, из строковой даты в формате ISO
     return sorted(
         lst_dct,
@@ -39,3 +44,21 @@ def sort_by_date(
         ),
         reverse=descending,
     )
+
+
+def is_wrong_parameter(lst_dct: list[dict[str, Union[str, int]]]) -> bool:
+    """Вспомогательная функция для проверки параметров"""
+    if not isinstance(lst_dct, list):
+        return True
+    else:
+        for dct in lst_dct:
+            if not isinstance(dct, dict):
+                return True
+            else:
+                for key, value in dct.items():
+                    if not (
+                        type(value) is str or type(value) is int
+                    ) or not isinstance(key, str):
+                        return True
+
+    return False
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,13 +1,71 @@
-# Функция mask_account_card
 import pytest
 
-from src.processing import filter_by_state
+from src.processing import filter_by_state, sort_by_date
+
+
+# Функция mask_account_card
+@pytest.mark.parametrize(
+    "data_fixture, state, excepted",
+    [
+        (
+            "right_list_1",
+            "CANCELED",
+            [
+                {
+                    "id": 594226727,
+                    "state": "CANCELED",
+                    "date": "2018-09-12T21:27:25.241689",
+                }
+            ],
+        ),
+        (
+            "right_list_2",
+            "EXECUTED",
+            [
+                {
+                    "id": 939719570,
+                    "state": "EXECUTED",
+                    "date": "2018-06-30T02:08:58.425572",
+                }
+            ],
+        ),
+        ("empty_list", "CANCELED", []),
+    ],
+)
+def test_filter_by_state(request, data_fixture, state, excepted):
+    data = request.getfixturevalue(data_fixture)
+    assert filter_by_state(data, state) == excepted
+
+
+def test_filter_by_state_empty(default_list):
+    assert filter_by_state(default_list) == [
+        {
+            "id": 41428829,
+            "state": "EXECUTED",
+            "date": "2019-07-03T18:35:29.512364",
+        }
+    ]
 
 
 @pytest.mark.parametrize(
-    "data, state, excepted",
+    "col_fixture",
+    ["wrong_type_col", "wrong_list", "wrong_dict_key", "wrong_dict_value"],
+)
+def test_filter_by_state_wrong_types(request, col_fixture):
+    with pytest.raises(TypeError) as exc_info:
+        data = request.getfixturevalue(col_fixture)
+        filter_by_state(data)
+
+    assert str(exc_info.value) == "Ошибка: некорректный тип входных данных."
+
+
+# Функция sort_by_date
+@pytest.mark.parametrize(
+    "data_fixture, descending, excepted",
     [
         (
+            "right_list_1",
+            True,
             [
                 {
                     "id": 41428829,
@@ -20,16 +78,10 @@ from src.processing import filter_by_state
                     "date": "2018-09-12T21:27:25.241689",
                 },
             ],
-            "CANCELED",
-            [
-                {
-                    "id": 594226727,
-                    "state": "CANCELED",
-                    "date": "2018-09-12T21:27:25.241689",
-                }
-            ],
         ),
         (
+            "right_list_2",
+            False,
             [
                 {
                     "id": 939719570,
@@ -42,84 +94,73 @@ from src.processing import filter_by_state
                     "date": "2018-10-14T08:21:33.419441",
                 },
             ],
-            "EXECUTED",
-            [
-                {
-                    "id": 939719570,
-                    "state": "EXECUTED",
-                    "date": "2018-06-30T02:08:58.425572",
-                }
-            ],
         ),
-        ([], "CANCELED", []),
+        ("empty_list", False, []),
     ],
 )
-def test_filter_by_state(data, state, excepted):
-    assert filter_by_state(data, state) == excepted
+def test_sort_by_date(request, data_fixture, descending, excepted):
+    data = request.getfixturevalue(data_fixture)
+    assert sort_by_date(data, descending) == excepted
 
 
-def test_filter_by_state_empty():
-    assert filter_by_state(
-        [
-            {
-                "id": 41428829,
-                "state": "EXECUTED",
-                "date": "2019-07-03T18:35:29.512364",
-            },
-            {
-                "id": 594226727,
-                "state": "CANCELED",
-                "date": "2018-09-12T21:27:25.241689",
-            },
-        ]
-    ) == [
+def test_sort_by_date_empty(default_list):
+    assert sort_by_date(default_list) == [
         {
             "id": 41428829,
             "state": "EXECUTED",
             "date": "2019-07-03T18:35:29.512364",
-        }
+        },
+        {
+            "id": 594226727,
+            "state": "CANCELED",
+            "date": "2018-09-12T21:27:25.241689",
+        },
     ]
 
 
 @pytest.mark.parametrize(
-    "data, state",
-    [
-        ({"key": "value"}, "CANCELED"),
-        (["key1", "key2"], "CANCELED"),
-        (
-            [
-                {
-                    15: 41428829,
-                    "state": "EXECUTED",
-                    "date": "2019-07-03T18:35:29.512364",
-                },
-                {
-                    "id": 594226727,
-                    "state": "CANCELED",
-                    "date": "2018-09-12T21:27:25.241689",
-                },
-            ],
-            "EXECUTED",
-        ),
-        (
-            [
-                {
-                    "id": 41428829,
-                    "state": True,
-                    "date": "2019-07-03T18:35:29.512364",
-                },
-                {
-                    "id": 594226727,
-                    "state": "CANCELED",
-                    "date": "2018-09-12T21:27:25.241689",
-                },
-            ],
-            "CANCELED",
-        ),
-    ],
+    "col_fixture",
+    ["wrong_type_col", "wrong_list", "wrong_dict_key", "wrong_dict_value"],
 )
-def test_filter_by_state_wrong_types(data, state):
+def test_sort_by_date_wrong_types(request, col_fixture):
     with pytest.raises(TypeError) as exc_info:
-        filter_by_state(data, state)
+        data = request.getfixturevalue(col_fixture)
+        sort_by_date(data)
 
     assert str(exc_info.value) == "Ошибка: некорректный тип входных данных."
+
+
+@pytest.mark.parametrize(
+    "data",
+    [
+        [
+            {
+                "id": 594226727,
+                "state": "CANCELED",
+                "date": "2018-09-12",
+            },
+            {
+                "id": 41428829,
+                "state": "EXECUTED",
+                "date": "2019/07/03T18:35:29.512364",
+            },
+        ],
+        [
+            {
+                "id": 594226727,
+                "state": "CANCELED",
+                "date": "2018.09.12",
+            },
+            {
+                "id": 41428829,
+                "state": "EXECUTED",
+                "date": "2019.12.15",
+            },
+        ],
+    ],
+)
+def test_sort_by_date_wrong_date(data):
+    with pytest.raises(ValueError) as exc_info:
+        sort_by_date(data)
+
+    assert str(exc_info.value) == "Ошибка: некорректный формат даты."
