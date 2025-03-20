1a8ee8d10701b36b113145d0b7f2c483bfbc4e54
ronnnnie85 <ronnnnie85@gmail.com>
2025-03-20 22:49:47 +0700

    сделал filter_by_currency и test

--- src/generators.py
+++ src/generators.py
@@ -10,7 +10,16 @@ def filter_by_currency(
     """Принимает на вход список словарей, представляющих транзакции.
     Возвращает итератор, который поочередно выдает транзакции,
     где валюта операции соответствует заданной."""
-    pass
+    filtered_transactions = filter(
+        lambda dct: dct.get("operationAmount", {})
+        .get("currency", {})
+        .get("code")
+        == currency,
+        lst_transactions,
+    )
+
+    for transaction in filtered_transactions:
+        yield transaction
 
 
 def transaction_descriptions(
@@ -24,4 +33,8 @@ def transaction_descriptions(
 
 def card_number_generator(begin: int, end: int) -> Iterator[str]:
     """Принимает начальное и конечное значения для генерации. Возвращает номера карт в заданном диапазоне"""
+    pass
+
+
+if __name__ == "__main__":
     pass
