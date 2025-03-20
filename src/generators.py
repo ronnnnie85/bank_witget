8f288d3de7105ac421ae6faf7dd741b1d1927166
ronnnnie85 <ronnnnie85@gmail.com>
2025-03-20 21:40:47 +0700

    добавил заготовку transaction_descriptions

--- src/generators.py
+++ src/generators.py
@@ -9,5 +9,14 @@ def filter_by_currency(
 ) -> Iterator[dict[str : int | str | dict[str : str | dict[str:str]]]]:
     """Принимает на вход список словарей, представляющих транзакции.
     Возвращает итератор, который поочередно выдает транзакции,
-    где валюта операции соответствует заданной"""
+    где валюта операции соответствует заданной."""
+    pass
+
+
+def transaction_descriptions(
+    lst_transactions: list[
+        dict[str : int | str | dict[str : str | dict[str:str]]]
+    ],
+) -> Iterator[str]:
+    """Принимает список словарей с транзакциями. Возвращает описание каждой операции по очереди."""
     pass
