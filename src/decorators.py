2d351daced90d794cec5e7e8fc1367456fe3c82c
ronnnnie85 <ronnnnie85@gmail.com>
2025-03-28 01:52:59 +0700

    Добавил функцию вывода сообщений

--- src/decorators.py
+++ src/decorators.py
@@ -1,4 +1,15 @@
 import functools
+import os
+
+
+def print_message(filename, text_message):
+    if filename is None:
+        print(text_message)
+    else:
+        current_dir = os.path.dirname(__file__)  # Папка, где лежит модуль
+        log_path = os.path.join(current_dir, "../logs/" + filename) # Абсолютный путь к файлу
+        with open(log_path, "a", encoding="utf-8") as f:
+            f.write(f"{text_message}\n")
 
 
 def log(filename=None):
