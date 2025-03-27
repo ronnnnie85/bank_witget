9bcc7a67365842614ea7b7dfcf4103c14b49bf06
ronnnnie85 <ronnnnie85@gmail.com>
2025-03-28 01:54:44 +0700

    Написал основную функцию

--- src/decorators.py
+++ src/decorators.py
@@ -16,6 +16,36 @@ def log(filename=None):
     def decorator(function):
         @functools.wraps(function)
         def wrapper(*args, **kwargs):
-            return function(*args, **kwargs)
+            #Блок запуска функции и получения лог сообщений
+            err = None
+
+            log_text_start = (
+                f"{strftime("[%Y-%m-%d %H:%M:%S]", localtime(time()))} "
+                f"{function.__name__} started with args: {args} "
+                f"and kwargs: {kwargs}"
+            )
+
+            print_message(filename, log_text_start)
+
+            try:
+                result = function(*args, **kwargs)
+                log_text_end = (
+                    f"{strftime("[%Y-%m-%d %H:%M:%S]", localtime(time()))} "
+                    f"{function.__name__} finished successfully "
+                    f"with result: {result}\n"
+                )
+
+                print_message(filename, log_text_end)
+
+                return result
+            except Exception as e:
+                log_text_end = (
+                    f"{strftime("[%Y-%m-%d %H:%M:%S]", localtime(time()))} "
+                    f"{function.__name__} error: {type(e).__name__}. "
+                    f"Args: {args} and kwargs: {kwargs}\n"
+                )
+
+                print_message(filename, log_text_end)
+                raise e
         return wrapper
     return decorator
