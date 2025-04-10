888ae2dc747cf480193e0beb459961fb35b5661e
ronnnnie85 <ronnnnie85@gmail.com>
2025-04-10 17:43:45 +0700

    создан модуль и функции четния из csv xlsx

--- src/reading_files.py
+++ src/reading_files.py
@@ -1,5 +1,15 @@
+import logging
+import os
+
 import pandas as pd
 
+from src import loggers
+
+name = os.path.splitext(os.path.basename(__file__))[0]
+file_name = os.path.join(
+    os.path.dirname(__file__), "..\\logs\\", f"{name}.log"
+)
+logger = loggers.create_logger(name, file_name, logging.DEBUG)
 
 def read_transactions_from_csv(file_path: str) -> list[dict]:
     """
@@ -8,10 +18,15 @@ def read_transactions_from_csv(file_path: str) -> list[dict]:
     try:
         df = pd.read_csv(file_path, delimiter=';')
     except FileNotFoundError as e:
+        logger.error(f"Ошибка: {e}")
         return []
     except ValueError as e:
+        logger.error(f"Ошибка: {e}")
         return []
     transactions = df.to_dict('records')
+    logger.info(
+        f"Файл {file_path} успешно обработан"
+    )
     return transactions
 
 
@@ -22,9 +37,14 @@ def read_transactions_from_excel(file_path: str) -> list[dict]:
     try:
         df = pd.read_excel(file_path)
     except FileNotFoundError as e:
+        logger.error(f"Ошибка: {e}")
         return []
     except ValueError as e:
+        logger.error(f"Ошибка: {e}")
         return []
 
     transactions = df.to_dict('records')
+    logger.info(
+        f"Файл {file_path} успешно обработан"
+    )
     return transactions
