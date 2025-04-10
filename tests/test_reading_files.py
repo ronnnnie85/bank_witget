8cf43b0959b582a4debbfc9a2c1dac4b7882252e
ronnnnie85 <ronnnnie85@gmail.com>
2025-04-10 17:49:57 +0700

    сделал тесты

--- tests/test_reading_files.py
+++ tests/test_reading_files.py
@@ -2,7 +2,7 @@ from unittest.mock import patch
 
 import pandas as pd
 
-from src.reading_files import read_transactions_from_csv
+from src.reading_files import read_transactions_from_csv, read_transactions_from_excel
 
 
 @patch("pandas.read_csv")
@@ -10,3 +10,28 @@ def test_transactions_from_csv(mock_csv, good_df):
     mock_csv.return_value = good_df
     assert read_transactions_from_csv("file.csv") == good_df.to_dict('records')
 
+
+@patch("pandas.read_csv")
+def test_read_transactions_from_csv_wrong_type(mock_csv):
+    mock_csv.side_effect = ValueError("Тестовая ошибка")
+    assert read_transactions_from_csv("file.csv") == []
+
+
+def test_read_transactions_from_csv_file_not_found():
+    assert read_transactions_from_csv("") == []
+
+
+@patch("pandas.read_excel")
+def test_transactions_from_xlsx(mock_xlsx, good_df):
+    mock_xlsx.return_value = good_df
+    assert read_transactions_from_excel("file.csv") == good_df.to_dict('records')
+
+
+@patch("pandas.read_excel")
+def test_read_transactions_from_xlsx_wrong_type(mock_xlsx):
+    mock_xlsx.side_effect = ValueError("Тестовая ошибка")
+    assert read_transactions_from_excel("file.csv") == []
+
+
+def test_read_transactions_from_xlsx_file_not_found():
+    assert read_transactions_from_excel("") == []
