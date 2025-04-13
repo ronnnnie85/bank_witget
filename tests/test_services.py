c8d5b791d2271d75a9e7cc66851d402437f25e87
ronnnnie85 <ronnnnie85@gmail.com>
2025-04-14 00:21:08 +0700

    сделал тесты

---
+++ tests/test_services.py
@@ -0,0 +1,23 @@
+from unittest.mock import patch
+
+import pytest
+
+from src.services import search_for_string, counting_operations
+
+
+def test_search_for_string(true_transactions):
+    assert search_for_string(true_transactions, "перевод") == true_transactions
+
+
+def test_search_for_string_empty_str(true_transactions):
+    with pytest.raises(ValueError, match="Пустая срока поиска"):
+        search_for_string(true_transactions, "")
+
+
+def test_counting_operations(true_transactions):
+    assert counting_operations(true_transactions, ["Перевод"]) == {"Перевод": 5}
+
+
+def test_counting_operations_empty_str(true_transactions):
+    with pytest.raises(TypeError, match="В списке категорий присутствуют не строки"):
+        assert counting_operations(true_transactions, [True])
