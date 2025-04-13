b0df6581cd686eb304fcdf0d83e6f2e9cb0eb785
ronnnnie85 <ronnnnie85@gmail.com>
2025-04-14 00:31:40 +0700

    заготовка main

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-from src.widget import get_date, mask_account_card
+def main():
+    pass
 
-print(mask_account_card("Maestro         1596837868705199"))
-print(mask_account_card("Счет 64686473678894779589"))
-print(mask_account_card("MasterCard 715830073476758"))
-print(mask_account_card("account 35383033474447895560"))
-print(mask_account_card("Visa Platinum 8990922113665229"))
-print(mask_account_card("Visa Gold 5999414228426353"))
 
-print(get_date("2024-03-11T02:26:18.671407"))
+if __name__ == "__main__":
+    main()
